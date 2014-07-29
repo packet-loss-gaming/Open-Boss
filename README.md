134a960fd68c23a9ede7e2ad1ac4996be80656d5
Wyatt Childers <theexcel@rocketmail.com>
2014-07-29 02:04:05 -0400

    Renamed the project to Open Boss

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-OSBL
+Open Boss
 ====
 
 Open Source MineCraft Boss Library
--- src/main/java/com/skelril/OSBL/util/DamageSource.java
+++ src/main/java/com/skelril/OSBL/util/DamageSource.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2014 Wyatt Childers.
  *
- * This file is part of OSBL.
+ * This file is part of Open Boss.
  *
- * OSBL is free software: you can redistribute it and/or modify
+ * Open Boss is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OSBL is distributed in the hope that it will be useful,
+ * Open Boss is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with OSBL.  If not, see <http://www.gnu.org/licenses/>.
+ * License along with Open Boss.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.skelril.OSBL.util;
