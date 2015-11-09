ef6780fb4746726ae2d2ac62ce123240d6ed479b
Wyatt Childers <wchilders@nearce.com>
2015-11-08 21:47:36 -0500

    Relicensed Open Boss under MPL

--- src/main/java/com/skelril/openboss/InstructionProcessor.java
+++ src/main/java/com/skelril/openboss/InstructionProcessor.java
@@ -1,20 +1,7 @@
 /*
- * Copyright (c) 2014 Wyatt Childers.
- *
- * This file is part of Open Boss.
- *
- * Open Boss is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Open Boss is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Open Boss.  If not, see <http://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 package com.skelril.openboss;
